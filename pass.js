72565202123b102d2bd37ec5c88001063a96773a
Rachel Yeung <rachel.cmyeung@gmail.com>
2019-12-01 12:13:16 -0800

    updated copy to clipboard function

--- pass.js
+++ pass.js
@@ -1,8 +1,8 @@
 confirm("Hi! Welcome to the password generator! Are you ready to create a new password?")
 
-var length = prompt("How long would you like your password to be? (Choose between 8-128 characters)")
-if(length < 8 || length > 128){
-    var length = prompt("Please enter a number between 8 to 128")
+var userLength = prompt("How long would you like your password to be? (Choose between 8-128 characters)")
+if(userLength < 8 || userLength > 128){
+    var userLength = prompt("Please enter a number between 8 to 128")
 }
 
 var special = confirm("Would you like it to contain special characters?")
@@ -16,3 +16,222 @@ if(special === false && number === false && lowerCase === false && upperCase ===
     var lowerCase = confirm("Would you like it to contain lowercase characters?")
     var upperCase = confirm("Would you like it to contain uppercase characters?")
 }
+
+
+if (special === true && number === false && lowerCase === false && upperCase === false){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "!#$%&^@*()+-_{}[]`~/|<>";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === false && number === true && lowerCase === false && upperCase === false){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "1234567890";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === false && number === false && lowerCase === true && upperCase === false){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "abcdefghijklmnopqrstuvwxyz";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === false && number === false && lowerCase === false && upperCase === true){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === true && number === true && lowerCase === false && upperCase === false){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "!#$%&^@*()+-_{}[]`~/|<>1234567890";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === true && number === false && lowerCase === true && upperCase === false){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "!#$%&^@*()+-_{}[]`~/|<>abcdefghijklmnopqrstuvwxyz";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === true && number === false && lowerCase === false && upperCase === true){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "!#$%&^@*()+-_{}[]`~/|<>ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === false && number === true && lowerCase === true && upperCase === false){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "1234567890abcdefghijklmnopqrstuvwxyz";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === false && number === true && lowerCase === false && upperCase === true){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+} 
+else if (special === false && number === false && lowerCase === true && upperCase === true){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}  
+else if (special === true && number === true && lowerCase === true && upperCase === false){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "!#$%&^@*()+-_{}[]`~/|<>1234567890abcdefghijklmnopqrstuvwxyz";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === true && number === true && lowerCase === false && upperCase === true){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "!#$%&^@*()+-_{}[]`~/|<>1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === true && number === false && lowerCase === true && upperCase === true){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "1#$%&^@*()+-_{}[]`~/|<>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}  
+else if (special === false && number === true && lowerCase === true && upperCase === true){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+     }
+     
+     document.getElementById("userpass").textContent = makepassword(userLength);
+}
+else if (special === true && number === true && lowerCase === true && upperCase === true){
+    function makepassword(length) {
+        var result           = "";
+        var characters       = "1#$%&^@*()+-_{}[]`~/|<>1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+        
+     }
+     document.getElementById("userpass").textContent = makepassword(userLength);
+     
+}
+else{
+    alert("Please pick at least one character type");
+}
+function copyClipboard(){
+    var range = document.createRange();
+    range.selectNode(document.getElementById("userpass"))
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+}
+
+// function copyClipboard(){
+//     var copyText = document.getElementById("userpass");
+//     copyText.select();
+//     document.execCommand("copy");
+//     alert("Copied the text: " + copyText.value) 
+// }
+
+
+// double check everything, sometimes the random doesnt give at least one type of every character
