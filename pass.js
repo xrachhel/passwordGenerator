bc13314cb023ffa4c6c434cdec066c49554b2918
Rachel Yeung <rachel.cmyeung@gmail.com>
2019-12-04 14:41:32 -0800

    final final push

--- pass.js
+++ pass.js
@@ -1,10 +1,4 @@
 function passwordButton() {
-
-    // var yesNo = confirm("Hi! Welcome to the password generator! Are you ready to create a new password?")
-    // if (yesNo === false) {
-    //     alert("Awww hope you come back soon!")
-    // }
-    // else {
         var userLength = prompt("How long would you like your password to be? (Choose between 8-128 characters)")
         if (userLength < 8 || userLength > 128) {
             var userLength = prompt("Please enter a number between 8 to 128")
@@ -218,11 +212,10 @@ function passwordButton() {
                 document.getElementById("userpass").textContent = makepassword(userLength);
 
             }
-            // else {
-            //     alert("Please pick at least one character type");
+            
         }
     }
-// }
+
 function copyClipboard() {
     var range = document.createRange();
     range.selectNode(document.getElementById("userpass"))
